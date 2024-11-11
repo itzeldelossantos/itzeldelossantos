c852860588fd8badaadad05605a27f965bd371d2
itzeldelossantos <L24230233@minatitlan.tecnm.mx>
2024-11-11 09:42:43 -0600

    problema9.py

---
+++ problema9.py
@@ -0,0 +1,7 @@
+num = int(input("Ingrese un valor: "))
+if num > 0:
+    print("El número es positivo")
+elif num < 0:
+    print("El número es negativo")
+else:
+    print("El número es igual a cero")
